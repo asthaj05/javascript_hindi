30af966da3261ed80620f8cbd207642cae166b37
asthaj05 <aasthajain0502@gmail.com>
2024-05-08 21:55:50 +0000

    datatypes of JAVASCRIPT

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict";  //treat all js code as newer version
+
+// alert(3+3) //we are using node.js not browser
+
+console.log(3 
+    +
+    3) // code readability should be high
+
+    console.log("hitesh")
+
+
+let name = "astha"
+let age = 3
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigInt => 
+// string => ""
+//boolean
+// null => standalone value 
+// undefined =>
+// symbol  => unique
+
+// object 
+
+console.log(typeof age)
+console.log(typeof null)
+
+console.log(typeof undefined)
+
+    
