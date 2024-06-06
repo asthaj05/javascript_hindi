f5326c59445db872aa6b497d424a37902abe5172
asthaj05 <aasthajain0502@gmail.com>
2024-06-06 19:26:28 +0530

    Create one.js

---
+++ 04_controlflow/one.js
@@ -0,0 +1,78 @@
+// if
+// const isUserLoggedIn = true
+// const temperature = 41
+
+// if(temperature === 40){
+//     console.log("less than 50");
+// }else{
+//     console.log("Temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// < , >, <=, >=, ==, !=, ===, 
+
+// const score = 200
+// if(score>100){
+//     const power = "fly"
+//     console.log(`User power : ${power}`);
+// }
+
+// console.log(`User power : ${power}`);
+
+
+//const balance = 1000
+
+// if(balance > 500)  console.log("test"), console.log("test2");
+
+
+// if(balance<500){
+//     console.log("less thn 500");
+// }
+// else if(balance<750){
+//     console.log("LESS THAN 750");
+// }
+// else{
+//     console.log("less than 1200");
+// }
+
+
+const userLoggenIn =  true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+// if(userLoggenIn && debitCard && 2==3){
+//     console.log("allow to buy courses");
+// }
+// if(loggedInFromEmail || loggedInFromGoogle){
+//     console.log("User logged in");
+// }
+
+// switch case
+
+const month = "May";
+
+switch(month){
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("FEbuary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case "May":
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
